9980062ae41d4f71d81d762ce3a60625b7937bf1
chaiwat2021 <79843590+chaiwat2021@users.noreply.github.com>
2021-03-02 09:20:43 +0700

    Update for example of casting variable

--- variable.py
+++ variable.py
@@ -1,4 +1,16 @@
+#normal declare
 x = 5
 y = "John"
 print(x)
 print(y)
+
+print("\n")
+
+#casting
+x = str(3) #for string
+y = int(3) #for integer
+z= float(3.0)  #for float
+
+print(x)
+print(y)
+print(z)
