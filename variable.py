de0dece60b851d94809447f3b44e1985a16d99d5
chaiwat2021 <79843590+chaiwat2021@users.noreply.github.com>
2021-03-02 09:30:55 +0700

    Update for example of multiple assign values

--- variable.py
+++ variable.py
@@ -3,7 +3,6 @@ x = 5
 y = "John"
 print(x)
 print(y)
-
 print("\n")
 
 #casting
@@ -11,6 +10,13 @@ x = str(3) #for string
 y = int(3) #for integer
 z= float(3.0)  #for float
 
+print(x)
+print(y)
+print(z)
+print("\n")
+
+#assign to multiple values
+x, y, z = "Orange", "Banana", "Cherry"
 print(x)
 print(y)
 print(z)
