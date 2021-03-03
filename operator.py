73f6074d21079976097f07f3715a874d2210483a
chaiwat2021 <79843590+chaiwat2021@users.noreply.github.com>
2021-03-03 08:59:11 +0700

    Create file for example of operator

---
+++ operator.py
@@ -0,0 +1,36 @@
+# assign value
+x, y, z = 4, 3, 2
+a, b, c = 1, 2, 3
+
+# Addition (+)
+print(x + y)
+
+# Subtraction (-)
+print(x - y)
+
+# Multiplication (*)
+print(x * y)
+
+# Division (/)
+print(x / y)
+print()
+
+# Comparison
+print(x == a)
+print(x != a)
+print(y > b)
+print(y < b)
+print(z >= c)
+print(z <= c)
+print()
+
+# Logical
+print(x > a and x < a)
+print(x > a or x < a)
+print(not(x > a or x < a))
+print()
+
+# Identity
+print(x is c)
+print(y is c)
+print(y is not c)
