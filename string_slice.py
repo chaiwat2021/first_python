8a474a08bbc189624fcbfd2a0bce0a0e8c0d7d21
chaiwat2021 <79843590+chaiwat2021@users.noreply.github.com>
2021-03-04 10:31:26 +0700

    Create file for example to slice string

---
+++ string_slice.py
@@ -0,0 +1,13 @@
+# normal slice
+b = "Hello World"
+print(b[2:5])
+print()
+
+# slice from start index
+b = "Hello World"
+print(b[:5])
+
+# slice to the end
+b = "Hello World"
+print(b[2:])
+print()
