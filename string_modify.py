ad5bc05907570c1967ce0c983274859f046a8c54
chaiwat2021 <79843590+chaiwat2021@users.noreply.github.com>
2021-03-04 10:49:08 +0700

    Create file for example to modify string

---
+++ string_modify.py
@@ -0,0 +1,20 @@
+# upper case
+a = "Hello World"
+print(a.upper())
+
+# lower case
+a = "Hello World"
+print(a.lower())
+
+# remove white space
+a = " Hello World"
+print(a)
+print(a.strip())
+
+# replace
+a = "Hello World"
+print(a.replace("H", "J"))
+
+# split
+a = "Hello World"
+print(a.split(","))
