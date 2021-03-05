8ec32c636938d1759e1e2fa02700061f9cdd2373
chaiwat2021 <79843590+chaiwat2021@users.noreply.github.com>
2021-03-05 08:44:46 +0700

    Create file for example to remove value or delete list

---
+++ list_remove.py
@@ -0,0 +1,29 @@
+# normal remove
+thislist = ["apple", "banana", "cherry"]
+thislist.remove("banana")
+print(thislist)
+
+# remove with in index
+thislist = ["apple", "banana", "cherry"]
+thislist.pop(1)
+print(thislist)
+
+# remove from the end of the list
+thislist = ["apple", "banana", "cherry"]
+thislist.pop()
+print(thislist)
+
+# remove with del command
+thislist = ["apple", "banana", "cherry"]
+del thislist[0]
+print(thislist)
+
+# delete entire list
+thislist = ["apple", "banana", "cherry"]
+del thislist
+
+# clear list
+thislist = ["apple", "banana", "cherry"]
+thislist.clear()
+print(thislist)
+
