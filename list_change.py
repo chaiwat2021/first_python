e1c36884b02b48e7c3ab8d592baf0f9b84ae303a
chaiwat2021 <79843590+chaiwat2021@users.noreply.github.com>
2021-03-05 08:19:20 +0700

    Create file for example to change list

---
+++ list_change.py
@@ -0,0 +1,15 @@
+# notmal change
+thislist = ["apple", "banana", "cherry"]
+thislist[1] = "blackcurrant"
+print(thislist)
+
+# change with range
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+thislist[1:3] = ["blackcurrant", "watermelon"]
+print(thislist)
+
+# insert new item
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(2, "watermelon")
+print(thislist)
+
